f37fe63ad6805d27e711c72ca67a0f4d7624918b
wogara <weogara@gmail.com>
2024-02-22 20:37:23 -0500

    adding refs to timer

--- 01-starting-project/src/components/TimerChallenge.jsx
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({title, targetTime}) {
     const [timerExpired, setTimerExpired] = useState(false);
     const [timerStarted, setTimerStarted] = useState(false);
+
+    const timer = useRef();
     function handleStart(){
 
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
         }, targetTime*1000);
         setTimerStarted(true);
+    }
+    function handleStop(){
+        clearTimeout(timer.current)
     }
     return (<section className="challenge">
         <h2>{title}</h2>
@@ -17,7 +22,7 @@ export default function TimerChallenge({title, targetTime}) {
             {targetTime} second{targetTime >1 ? 's' : ''}
         </p>
         <p>
-            <button onClick={handleStart}>
+            <button onClick={timerStarted ? handleStop:handleStart}>
                 {timerStarted ? 'Stop': 'Start'} Challenge
             </button>
         </p>
